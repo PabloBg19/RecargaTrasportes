609ef375ad4f0efc52e95a211c9346883305eca5
PabloBg19 <pbelascoainguerra@gmail.com>
2025-10-07 19:52:58 +0200

    v0.2

--- RecargaTren/src/main/java/Interfaz/RenfeInterfaz.java
+++ RecargaTren/src/main/java/Interfaz/RenfeInterfaz.java
@@ -7,8 +7,9 @@ public class RenfeInterfaz {
         JFrame frame = new JFrame("RenfeInterfaz");
         frame.setContentPane(new RenfeInterfaz().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,420);
+        frame.setSize(770,590);
         frame.setVisible(true);
+        frame.setResizable(false);
     }
 
     private JPanel panel1;
@@ -18,5 +19,4 @@ public class RenfeInterfaz {
     private JSpinner spinner1;
     private JTextField textField3;
     private JButton accederButton;
-    private JComboBox comboBox1;
 }
