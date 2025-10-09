2ec34b248954e2e30ac0ee08402c7f437ea881ff
MiguelAngelPerez21 <CanonigoOnce11@>
2025-10-09 14:17:31 +0200

    Subida

--- Recarga_Bus/src/main/java/ElegirAbono.java
+++ Recarga_Bus/src/main/java/ElegirAbono.java
@@ -1,4 +1,7 @@
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class ElegirAbono
 {
@@ -8,11 +11,36 @@ public class ElegirAbono
     private JPanel Bono;
     private JLabel Logo;
     private JLabel Autobus;
+    private JLabel SeleccionarFecha;
+    private JPanel Fecha;
+    private JLabel Calendario;
+    private JPanel FechaInicio;
+    private JLabel Inicio;
+    private JPanel CalendarioInicio;
+    private JPanel FechaFin;
+    private JLabel Fin;
+    private JPanel CalendarioFin;
+    private JPanel Contenidos;
+    private JPanel Numero;
+    private JPanel Recargar;
+    private JTextField textField1;
     private JLabel NumeroTargeta;
-    private JTextField textFieldNumeroTargeta;
-    private JButton buttonRecargar;
-    private JPanel DatosTargeta;
-    private JPanel BotonRecargar;
+    private JButton recargarButton;
+
+    public ElegirAbono()
+    {
+        Targeta.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+
+        CalendarioInicio.setLayout(new BorderLayout());
+        JDateChooser dateChooserInicio = new JDateChooser();
+        CalendarioInicio.add(dateChooserInicio, BorderLayout.CENTER);
+        CalendarioInicio.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Ad
+
+        CalendarioFin.setLayout(new BorderLayout());
+        JDateChooser dateChooserFin = new JDateChooser();
+        CalendarioFin.add(dateChooserFin, BorderLayout.CENTER);
+        CalendarioFin.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("ElegirAbono");
